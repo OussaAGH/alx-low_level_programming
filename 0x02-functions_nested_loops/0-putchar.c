2729c58f2643d942c97886cec8987ce06e582cfa
Oussama aghzer <aghzerossama@gmail.com>
2022-12-13 07:37:56 -0800

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
-#include <main.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 
 {
@@ -17,6 +19,6 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 
 }
